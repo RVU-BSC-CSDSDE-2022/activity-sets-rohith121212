fe456f4d3645f1d5ade76d055c1388b225865af5
Rohith BSc <115694252+rohith121212@users.noreply.github.com>
2022-12-12 06:25:34 +0000

    problem11

---
+++ set01/problem11.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+struct complex
+{
+    float real;
+    float imaginary;
+};
+typedef struct complex Complex;
+
+Complex input_complex();
+Complex add_complex(Complex a, Complex b);
+void output(Complex a, Complex b, Complex sum);
+
+int main()
+{
+  Complex a,b, sum;
+   a=input_complex();
+   b=input_complex();
+   sum=add_complex(a,b);
+    output(a,b, sum);
+    return 0;
+}
+
+Complex input_complex()
+{
+  Complex c1;
+  printf("Enter the real part\n");
+  scanf("%f", &c1.real);
+  printf("Enter the imaginary part\n");
+  scanf("%f", &c1.imaginary);
+  return c1;
+}
+Complex add_complex(Complex a, Complex b)
+{
+  Complex sum;
+   sum.real=a.real+b.real;
+    sum.imaginary=       a.imaginary+b.imaginary;
+  return sum;
+}
+
+void output(Complex a, Complex b, Complex sum)
+{
